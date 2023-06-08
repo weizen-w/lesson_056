e719d8584f35f3fdd121173a5c9f5e4c4b146a6b
Wladimir <weizen.w@gmx.de>
2023-06-08 13:32:10 +0200

    feat: add Task1 solution

--- src/Task1.java
+++ src/Task1.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class Task1 {
 
   public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int a = scanner.nextInt();
+    System.out.println(reverseBinary(a));
+  }
+
+  public static String reverseBinary(int x) {
+    if (x < 0) {
+      throw new IllegalArgumentException("Число не может быть отрицательным: " + x);
+    }
+    if (x < 2) {
+      return String.valueOf(x);
+    }
+    return x % 2 + reverseBinary(x / 2);
   }
 }
