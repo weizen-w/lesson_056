aecb69ae0e1720f9194d42e4c274cb03357df53f
Wladimir <weizen.w@gmx.de>
2023-06-08 13:33:45 +0200

    feat: add Task1 solution for template

--- src/Task1.java
+++ src/Task1.java
@@ -4,17 +4,17 @@ public class Task1 {
 
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
-    int a = scanner.nextInt();
-    System.out.println(reverseBinary(a));
+    int num = scanner.nextInt();
+    System.out.println(reverse(num));
   }
 
-  public static String reverseBinary(int x) {
+  public static String reverse(int x) {
     if (x < 0) {
       throw new IllegalArgumentException("Число не может быть отрицательным: " + x);
     }
     if (x < 2) {
       return String.valueOf(x);
     }
-    return x % 2 + reverseBinary(x / 2);
+    return x % 2 + reverse(x / 2);
   }
 }
